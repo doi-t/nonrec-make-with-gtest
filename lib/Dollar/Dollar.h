f5fc04ae47c3808d660e04020eefa65d8115dc54
Doi Toshiya <doi.t.npdp@gmail.com>
2014-02-16 17:05:17 +0900

    p30,31:Francのリファクタリング, TODOを更新 等価演算子==を一般化 テストはグリーン
    Javaでは、Franc.equals()とMoney.equals()で全く同じ実装を行ってテストを通した上で、
    Francの冗長な実装を削除している

--- lib/Dollar/Dollar.h
+++ lib/Dollar/Dollar.h
@@ -26,10 +26,6 @@ public:
 	Franc times(int multiplier){
 		return Franc(amount * multiplier);
 	}
-
-	bool operator==(const Franc& other) const {
-		return this->amount == other.amount;
-	}
 };
 
 #endif //___Dollar
