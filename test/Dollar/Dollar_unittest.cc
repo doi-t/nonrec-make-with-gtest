9e33fd71f179dd6482e8dab085889aaab1788fe1
Doi Toshiya <doi.t.npdp@gmail.com>
2014-02-16 14:39:47 +0900

    WIP:テストがコンパイルエラー - p19,20:等価性の定義によって、テストをより「情報豊富な」ものにする
    本文中では、equalsメソッドの実装をもって等価性を定義したと書いているが、C++ではそんなわけなかった
    
    JunitのassertEqualsの実装の中を見ると、内部でequalsメソッドを呼び出して比較している
    下記リンク先のgithub参照
    https://stackoverflow.com/questions/13607597/does-assertequalsobject-o1-object-o2-uses-the-equals-method

--- test/Dollar/Dollar_unittest.cc
+++ test/Dollar/Dollar_unittest.cc
@@ -4,9 +4,9 @@
 TEST(Dollar, testMultiplication){
 	Dollar five = Dollar(5);
 	Dollar product = five.times(2);
-	EXPECT_EQ(10, product.amount);
+	EXPECT_EQ(Dollar(10), product);
 	product = five.times(3);
-	EXPECT_EQ(15, product.amount);
+	EXPECT_EQ(Dollar(15), product);
 }
 
 TEST(Dollar, testEquality){
